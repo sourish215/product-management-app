ddc3ec330edf3cb04728855866ec1d13786a4274
sourish215 <sourish@zapscale.com>
2025-03-25 11:58:53 +0530

    feat: configure nuxt config, add environment variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// nuxt.config.js
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  css: ["vuetify/styles", "@mdi/font/css/materialdesignicons.css"],
+
+  build: {
+    transpile: ["vuetify"],
+  },
+
+  ssr: true,
+
+  modules: [],
+
+  app: {
+    head: {
+      title: "Product Management",
+      titleTemplate: "%s - Product Management App",
+    },
+  },
+
+  compatibilityDate: "2025-03-24",
+
+  runtimeConfig: {
+    AWS_REGION: process.env.AWS_REGION || "local",
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
